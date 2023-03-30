a7a0d7d515638e17fe3bf742cdc256098af4311e
arrancurran <arrancurran@gmail.com>
2023-03-30 11:05:34 +0200

    noise

--- tests/noise_on_pos.m
+++ tests/noise_on_pos.m
@@ -96,8 +96,8 @@ disp( [ num2str( mean( x_nf ) ), ' - Unfiltered Mean' ] )
 disp( [ num2str( std( x_f ) ), ' - Filtered Variance ' ] )
 disp( [ num2str( mean( x_f ) ), ' - Filtered Mean ' ] )
 
-img_out_noise = imresize( img_noise,  [ 512 512 ] ) ;
-img_out_filt = imresize( img_filt,  [ 512 512 ] ) ;
+img_out_noise = imresize( img_noise,  [ 100 100 ] ) ;
+img_out_filt = imresize( img_filt,  [ 100 100 ] ) ;
 
 imwrite( uint8( img_out_noise ), 'img/img_in_noisy.jpg', 'jpeg' ) ;
 imwrite( uint8( img_out_filt ), 'img/img_in_noisy_filtered.jpg', 'jpeg' ) ;
