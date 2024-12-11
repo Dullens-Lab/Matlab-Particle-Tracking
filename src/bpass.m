015e9267f027f91ab53b1a2a0e5970d13ae80d87
arrancurran <arrancurran@gmail.com>
2024-12-11 12:48:28 +0100

    removed origanal

--- src/bpass.m
+++ src/bpass.m
@@ -17,71 +17,6 @@
 %
 %`img_hpass` and `img_lpass` can be returned with `[ img_out, img_hpass ] = bpass()` and `[ img_out, ~, img_lpass ] = bpass()`, respectively.
 
-%{
-
-Notes on convolution:
-
-JWM: Do a 2D convolution with the kernels in two steps each. It is
-possible to do the convolution in only one step per kernel with 
-
-  lpass_conv = conv2(lpass_kernel',lpass_kernel,image,'same');
-  boxcar_conv = conv2(box_kernel', box_kernel,image,'same');
-
-but for some reason, this is slow. The whole operation could be reduced
-to a single step using the associative and distributive properties of
-convolution:
-
-  filtered = conv2(image,...
-    lpass_kernel'*lpass_kernel - box_kernel'*box_kernel,...
-    'same');
-
-But this is also comparatively slow (though inexplicably faster than the
-above). It turns out that convolving with a column vector is faster than
-convolving with a row vector, so instead of transposing the kernel, the
-image is transposed twice.
-
-This is still true as of 2023. Also imgaussfilt() is also slower and has the effect of translating the centroids in X and Y
-
-CHANGELOG:
-
-Feb 1993
-Written by David G. Grier, The University of Chicago.
-
-May 1995
-Greatly revised version DGG.
-
-Dec 1995
-Added /field keyword JCC.
-
-Aug 1999
-Memory optimizations and fixed normalization, DGG.
-
-Apr 2004-ish
-Converted to Matlab by D.Blair.
-
-June 2005
-Fixed some bugs with conv2 to make sure the edges are removed D.B.
-Removed inadvertent image shift ERD.
-
-Aug 24 2005
-Added threshold to output. Now sets all pixels with negative values equal to zero. Gets rid of ringing 
-which was destroying sub-pixel accuracy, unless window size in cntrd was picked perfectly. Now centrd 
-gets sub-pixel accuracy much more robustly ERD.
-
-Jun 2007
-Refactored for clarity and converted all convolutions to use column vector kernels for speed. Running
-on my  macbook, the old version took ~1.3 seconds to do bpass(image,1,19) on a 1024 x 1024 image;
-this version takes roughly half that. JWM
-
-Jan 2023
-Reformated to meet commenting and nomenclecture standards. AC
-Added image scalling on input and ouptut so we process with full bandwidth (0-255) and we return full bandwidth. AC
-Added check to see if image in has already been converted to 'double'. AC
-Removed edge zero-ing as this happens in pkfnd() by removing peaks within edge rather than blacking out and potentially 
-picking parts of particles.
-
-%}
-
 function [ img_out, img_hpass, img_lpass ] = bpass( img_in, hpass, lpass, backgrnd, display )
 
     if nargin < 4
@@ -94,17 +29,16 @@ function [ img_out, img_hpass, img_lpass ] = bpass( img_in, hpass, lpass, backgr
     % Convert to double 
     if isa( img_in, 'double' ) ~= 1, img_in = double( img_in ) ; end
 
-    normalize   = @( x ) x ; % x / sum( x ) ;
-    scale2init8 = @( x ) x ; % ( x - min( x, [], 'all' ) ) ./ max( ( x - min( x, [], 'all' ) ), [], 'all' ) * 255 ;
+    normalize   = @( x ) x / sum( x ) ;
+    scale2init8 = @( x ) ( x - min( x, [], 'all' ) ) ./ max( ( x - min( x, [], 'all' ) ), [], 'all' ) * 255 ;
     
-    % TODO: This can be problematic in the scenario where we have a group
+    % NOTE: This can be problematic in the scenario where we have a group
     % of images but some images contain no objects. In this case we scale
     % the noise to 255 and therefore pick up these bright noisy pixels
 
     img_in      = scale2init8( img_in ) ;
     img_out     = img_in ;
     
-        
     %%%     High Pass Filter    %%%
     
     % The kernel is designed to increase the brightness of the center pixel relative to neighboring pixels.
@@ -116,7 +50,6 @@ function [ img_out, img_hpass, img_lpass ] = bpass( img_in, hpass, lpass, backgr
     %   -1/9    -1/9    -1/9
     %
     % https://www.l3harrisgeospatial.com/docs/highpassfilter.html
-    % TODO: Explore effect of amplitude on kernel
     if hpass
         box_kernel  = - ones( 3 ) / 9 ; box_kernel( 2, 2 ) = 8 / 9 ;
         img_hpass   = conv2( img_out, box_kernel, 'same' ) ;
@@ -142,7 +75,6 @@ function [ img_out, img_hpass, img_lpass ] = bpass( img_in, hpass, lpass, backgr
             nop     = [ nop_bld ; - nop_bld * 2 ; nop_bld ] ;
 
             [ img_rows, img_cols ] = size( img_out ) ;
-            % TODO: Should we calc nop from the raw input regardless if hpass has happened?
             nop_sigma   = sum( abs( conv2( img_out, nop ) ), 'all'  ) ;
             lpass       = round( nop_sigma * sqrt( .5 * pi ) / ( 6 * ( img_rows - 2 ) * ( img_cols - 2 ) ) ) ;
         end
@@ -150,8 +82,6 @@ function [ img_out, img_hpass, img_lpass ] = bpass( img_in, hpass, lpass, backgr
         if lpass ~= 1 % Dont waste my time with good images!
             lpass_x      = - lpass : lpass ;
             lpass_kernel = normalize( exp( -( lpass_x / ( 2 * lpass ) ) .^2 ) ) ;
-            % TODO: A comment from https://github.com/zaptowicz/Particle-Tracking-using-Matlab/blob/main/bpass.m line 77
-            % suggests setting mean(img) == 0 reduces artifacts, explore this.
             img_lpass    = conv2( img_out, lpass_kernel, 'same' ) ;
             img_lpass    = conv2( img_lpass, lpass_kernel', 'same' ) ;
             img_lpass    = scale2init8( img_lpass ) ;
